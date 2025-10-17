de5321dd812d6b2d93c22f223261b272c03dc419
bth <contactbth@gmail.com>
2025-10-17 17:03:57 +0200

    Set window always on top and translucent

--- pvcscr/__main__.py
+++ pvcscr/__main__.py
@@ -1,19 +1,21 @@
 import sys
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow
 
 class PvcScr(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setWindowTitle("PvcScr")
+        self.setWindowFlags(self.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
+        self.setAttribute(Qt.WidgetAttribute.WA_NoSystemBackground, True)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
 
 def main():
     print("pvcscr")
     app = QApplication(sys.argv)
-    #label = QLabel("Hello World", alignment=Qt.Alignment.AlignCenter)
-    #label.show()
     window = PvcScr()
     window.show()
+    # Run the main Qt loop
     sys.exit(app.exec())
 
 if __name__ == "__main__":
