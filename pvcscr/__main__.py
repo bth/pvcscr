1696223f8a6cc0f537ada8eab189462abcedff6f
bth <contactbth@gmail.com>
2025-10-17 10:29:51 +0200

    First window with custom class

--- pvcscr/__main__.py
+++ pvcscr/__main__.py
@@ -1,12 +1,19 @@
 import sys
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
+
+class PvcScr(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setWindowTitle("PvcScr")
 
 def main():
     print("pvcscr")
     app = QApplication(sys.argv)
-    label = QLabel("Hello World", alignment=Qt.Alignment.AlignCenter)
-    label.show()
+    #label = QLabel("Hello World", alignment=Qt.Alignment.AlignCenter)
+    #label.show()
+    window = PvcScr()
+    window.show()
     sys.exit(app.exec())
 
 if __name__ == "__main__":
