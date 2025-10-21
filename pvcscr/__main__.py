dcd00a1c3c1bc6852f9d921bee9243978d122998
bth <contactbth@gmail.com>
2025-10-21 20:05:41 +0200

    Experimentation of transparent window

--- pvcscr/__main__.py
+++ pvcscr/__main__.py
@@ -1,6 +1,7 @@
 import sys
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Qt, QRect, QPoint, QSize
+from PySide6.QtWidgets import QApplication, QMainWindow, QFrame
+from PySide6.QtGui import QRegion
 
 class PvcScr(QMainWindow):
     def __init__(self):
@@ -9,6 +10,16 @@ class PvcScr(QMainWindow):
         self.setWindowFlags(self.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
         self.setAttribute(Qt.WidgetAttribute.WA_NoSystemBackground, True)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
+        self.move(100, 100)
+        self.frame = QFrame(self)
+        self.frame.setFrameStyle(1)
+        self.frame.setStyleSheet("QFrame { border: 3px solid red;}")
+
+    def resizeEvent(self, event):
+        self.frame.setGeometry(0, 0, event.size().width(), event.size().height())
+        empty_region = QRegion(QRect(QPoint(2,2), self.frame.size() - QSize(4, 4)), QRegion.RegionType.Rectangle)
+        region = QRegion(QRect(QPoint(-2,-2), self.frame.size() + QSize(4, 4)), QRegion.RegionType.Rectangle)
+        self.setMask(region - empty_region)
 
 def main():
     print("pvcscr")
