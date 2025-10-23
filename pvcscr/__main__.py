cc58ac9fba6f80ebb557b24f6fa2b6edb0ebc391
bth <contactbth@gmail.com>
2025-10-23 18:31:01 +0200

    Experiment mouse tracker

--- pvcscr/__main__.py
+++ pvcscr/__main__.py
@@ -5,7 +5,7 @@ from PySide6.QtGui import QRegion
 
 class PvcScr(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
+        super().__init__()
         self.setWindowTitle("PvcScr")
         self.setWindowFlags(self.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
         self.setAttribute(Qt.WidgetAttribute.WA_NoSystemBackground, True)
@@ -13,8 +13,9 @@ class PvcScr(QMainWindow):
         self.move(100, 100)
         self.frame = QFrame(self)
         self.frame.setFrameStyle(1)
-        self.frame.setStyleSheet("QFrame { border: 3px solid red;}")
-
+        self.frame.setStyleSheet("QFrame { border: 5px solid red;}")
+        self.setMouseTracking(True)
+        self.frame.setMouseTracking(True)
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.time)
         self.timer.start(1000)
@@ -22,8 +23,13 @@ class PvcScr(QMainWindow):
     def resizeEvent(self, event):
         self.frame.setGeometry(0, 0, event.size().width(), event.size().height())
 
+    def mouseMoveEvent(self, event):
+        # Position relative à la fenêtre
+        local_pos = event.pos()
+        print(f"Position locale dans la fenêtre : {local_pos.x()}, {local_pos.y()}")
+
     def time(self):
-        empty_region = QRegion(QRect(QPoint(2,2), self.frame.size() - QSize(4, 4)), QRegion.RegionType.Rectangle)
+        empty_region = QRegion(QRect(QPoint(5,5), self.frame.size() - QSize(10, 10)), QRegion.RegionType.Rectangle)
         region = QRegion(QRect(QPoint(-2,-2), self.frame.size() + QSize(4, 4)), QRegion.RegionType.Rectangle)
         self.setMask(region - empty_region)
 
